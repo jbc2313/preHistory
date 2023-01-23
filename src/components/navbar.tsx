4cff9ae75c3b381535caea203580cc82da5d6db0
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-01-23 10:31:22 -0600

    added css to nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,14 @@
 import styles from '@/styles/Nav.module.css'
+import Link from 'next/link'
 
 export default function Navbar() {
     return (
         <div className={styles.main}>
-            <ul>
-                <li>Home</li> 
-                <li>Sites</li>
-            </ul>
+            <div className={styles.linkDiv}>
+                <Link className={styles.Link} href={'/'}>Home</Link> 
+                <Link className={styles.Link} href={'/sites'}>Sites</Link>
+                <Link className={styles.Link} href={'/worldmap'}>World</Link>
+            </div>
         </div>
 
     )
