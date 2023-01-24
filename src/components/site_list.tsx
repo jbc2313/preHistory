206f7c1ec7a09a31686ee913bb44748f2350bb4c
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-01-24 15:32:15 -0600

    sitelist compnonent added

---
+++ src/components/site_list.tsx
@@ -0,0 +1,15 @@
+
+import styles from '@/styles/SiteList.module.css'
+
+export default function SiteList() {
+    
+    return (
+        <div className={styles.SiteListMain}>
+            Site List Main Div
+
+
+        </div>
+
+    )
+
+}
