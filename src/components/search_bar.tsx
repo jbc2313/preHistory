5af5d77c6395890f5228d3e55a3e30715df21b04
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-07-18 21:48:58 -0500

    updated search bar to save input

--- src/components/search_bar.tsx
+++ src/components/search_bar.tsx
@@ -3,14 +3,21 @@ import styles from '@/styles/Search.module.css'
 
 
 export default function SearchBar() {
-    const [searchtext, setSearchText] = useState(""); 
+    const [searchtext, setSearchText] = useState<string | number | readonly string[]>();
 
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchText(e.target.value)
+    }
+
+    const dontSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+    }
 
     return (
         <div className={styles.main}>
             <p>Search</p>
-            <form className={styles.form}>
-                <input value={searchtext} type="text" />
+            <form className={styles.form} onSubmit={dontSubmit}>
+                <input value={searchtext} type="text" onChange={handleSearch} />
             </form>
         </div>
     )
