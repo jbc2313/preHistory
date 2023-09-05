3c91686419b8fcc452d6fb6b814ebfce975c4e2b
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-09-05 09:00:00 -0500

    updated footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,8 +4,8 @@ import styles from '@/styles/Footer.module.css'
 export default function Footer() {
     return (
         <div className={styles.main}>
-            <p>Copyright TRUEHISTORY(2023)</p>
-            <p> "LEARN WHILE YOU CAN"</p>
+            <p>Copyright TRUEHISTORY 2023</p>
+            <p>"LEARN WHILE YOU CAN"</p>
         </div>
 
     )
