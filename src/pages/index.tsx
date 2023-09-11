2458b5c6fe0ffd45132363b2ff5091ad6228809c
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-09-11 09:00:00 -0500

    updated text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,11 +15,11 @@ export default function Home() {
         <div className={styles.mainDiv}>
             <h1>Welcome to Pre"History"..</h1>
            
-            <p>What if I told you modern historians have been wrong..</p>
-            <p>What if I told you mankind is much older then we thought..</p>
+            <p>What if I told you modern historians have been wrong.</p>
+            <p>What if I told you mankind is much older then we thought.</p>
             <p>This is website where facts are the only thing that matter.</p>
-            <p>Where everything you learn here, supposedly never happened..</p>
-
+            <p>Where everything you learn here, supposedly never happened.</p>
+            
             <Image src={"/thirteen.svg"} alt={"13"} width={500} height={500} />
         </div>
       </div>
